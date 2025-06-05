e4c3d6a865e6118c514af11c8553762adbb17127
shawn-dev <galdoshawn24@gmail.com>
2025-06-05 10:37:22 +0800

    Add files via upload

---
+++ C#/Day 1 - Day 3/Day 9/Day 9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 9: Store names in an array and search for a specific name.");
+            string[] info = { "Shawn", "Marl", "Amaya", "Juan" };
+
+            string name = "Marl";
+            bool found = false;
+            for (int i = 0; i < info.Length; i++)
+            {
+                if(name == info[i])
+                {
+                    Console.WriteLine("Search Found");
+                    found = true;
+                    break;
+                }
+        
+            }
+            if (!found)
+            {
+                Console.WriteLine("Search not found");
+            }
+        }
+    }
+}
