131d0e903eaa36603ae46d5166797a216b85842e
shawn-dev <galdoshawn24@gmail.com>
2025-06-05 11:08:39 +0800

    Add files via upload

---
+++ C#/Day 1 - Day 3/Day 10/Day 10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Day 10: Display the multiplication table of a number entered by the user.");
+
+            Console.Write(" Enter a number : ");
+            int num = int.Parse(Console.ReadLine());
+
+            int ans;
+
+            for(int i = num; i <= num; i++)
+            {
+                //Console.WriteLine(i);
+                for (int j = 1; j <= 10; j++)
+                {
+                    ans = i * j;
+                    Console.WriteLine(i + " * " + j + " = " + ans);
+                }
+
+            }
+        }
+    }
+}
